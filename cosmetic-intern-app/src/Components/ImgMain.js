a686efe12281c1f41b6d68fe4f870e772947db84
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-25 10:06:08 +0545

    commit

--- cosmetic-intern-app/src/Components/ImgMain.js
+++ cosmetic-intern-app/src/Components/ImgMain.js
@@ -2,9 +2,12 @@ import ImgCSS from "./ImgMain.module.css";
 import React, { createRef, useState } from "react";
 import HeroSlider, { Slide } from "hero-slider";
 
-const bogliasco = "https://i.imgur.com/Gu5Cznz.jpg";
-const countyClare = "https://i.imgur.com/idjXzVQ.jpg";
-const craterRock = "https://i.imgur.com/8DYumaY.jpg";
+const bogliasco =
+  "https://images.unsplash.com/photo-1503236823255-94609f598e71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80";
+const countyClare =
+  "https://images.unsplash.com/photo-1580870069867-74c57ee1bb07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80";
+const craterRock =
+  "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=887&q=80";
 const giauPass = "https://i.imgur.com/8IuucQZ.jpg";
 
 const ImgMain = () => {
@@ -12,7 +15,8 @@ const ImgMain = () => {
     <>
       <div className={ImgCSS.banner}>
         <HeroSlider
-          height={"100vh"}
+          width={"100vw"}
+          height={"80vh"}
           autoplay
           controller={{
             slidingAnimation: "left_to_right",
