0b82ab36506d504e0a91f1837750b93756de8b8f
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-26 11:40:46 +0545

    commit

--- cosmetic-intern-app/src/Components/MainImage/ImgMain.js
+++ cosmetic-intern-app/src/Components/MainImage/ImgMain.js
@@ -23,19 +23,19 @@ const ImgMain = () => {
           }}
         >
           <Slide
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            style={{ objectFit: "contain", width: "100%", height: "100%" }}
             background={{
               backgroundImageSrc: img1,
             }}
           />
           <Slide
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            style={{ objectFit: "contain", width: "100%", height: "100%" }}
             background={{
               backgroundImageSrc: img2,
             }}
           />
           <Slide
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            style={{ objectFit: "contain", width: "100%", height: "100%" }}
             background={{
               backgroundImageSrc: img3,
             }}
